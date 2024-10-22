4ab0fe02eb3b0fcc2dec198e777aa93531461625
gowtham-0116 <gowthamkomagan0116@gmail.com>
2024-10-22 15:55:15 +0530

    Update PRIORITY ENCODER WITH CASEZ.v

--- 5.PROCEDURES/PRIORITY ENCODER WITH CASEZ.v
+++ 5.PROCEDURES/PRIORITY ENCODER WITH CASEZ.v
@@ -2,24 +2,17 @@ module top_module (
     input [3:0] in,
     output reg [1:0] pos  );
     
-    always@(*) begin
-        case(in)
-            4'b0000 : pos = 1'b0;
-            4'b0001 : pos = 1'b0;
-            4'b0010 : pos = 2'b01;
-            4'b0011 : pos = 2'b0;
-            4'b0100 : pos = 2'b10;
-            4'b0101 : pos = 2'b0;
-            4'b0110 : pos = 2'b01;
-            4'b0111 : pos = 2'b0;
-            4'b1000 : pos = 2'b11;
-            4'b1001 : pos = 2'b0;
-            4'b1010 : pos = 2'b01;
-            4'b1011 : pos = 2'b0;
-            4'b1100 : pos = 2'b10;
-            4'b1101 : pos = 2'b0;
-            4'b1110 : pos = 2'b01;
-            4'b1111 : pos = 2'b0;
+      always@(*) begin
+        casez(in[7:0])
+            8'bzzzzzzz1 : pos=0;
+            8'bzzzzzz1z : pos=1;
+            8'bzzzzz1zz : pos=2;
+            8'bzzzz1zzz : pos=3;
+            8'bzzz1zzzz : pos=4;
+            8'bzz1zzzzz : pos=5;
+            8'bz1zzzzzz : pos=6;
+            8'b1zzzzzzz : pos=7;
+            default : pos=0;
         endcase
     end
 endmodule
