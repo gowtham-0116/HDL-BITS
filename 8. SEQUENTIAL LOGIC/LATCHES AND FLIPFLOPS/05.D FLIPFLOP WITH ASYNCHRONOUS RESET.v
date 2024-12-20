7d153690b1374e2f778cc752b29e78f06f474cce
Gowtham <gowthamkomagan0116@gmail.com>
2024-12-20 15:53:15 +0530

    Create 05.D FLIPFLOP WITH ASYNCHRONOUS RESET.v

---
+++ 8. SEQUENTIAL LOGIC/LATCHES AND FLIPFLOPS/05.D FLIPFLOP WITH ASYNCHRONOUS RESET.v
@@ -0,0 +1,11 @@
+module top_module (
+    input clk,
+    input areset,   // active high asynchronous reset
+    input [7:0] d,
+    output [7:0] q
+);
+    always@(posedge clk or posedge areset) begin
+        if(areset == 1'b1) q<=1'b0;
+        else q<=d;
+    end
+endmodule
