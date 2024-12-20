23d3532acbfbf64f806d65d992b6d4efaec7ffaf
Gowtham <gowthamkomagan0116@gmail.com>
2024-12-20 15:50:19 +0530

    Create 03.D FLIPFLOP WITH RESET.v

---
+++ 8. SEQUENTIAL LOGIC/LATCHES AND FLIPFLOPS/03.D FLIPFLOP WITH RESET.v
@@ -0,0 +1,11 @@
+module top_module (
+    input clk,
+    input reset,            // Synchronous reset
+    input [7:0] d,
+    output [7:0] q
+);
+    always@(posedge clk) begin
+        if(reset) q[7:0]<=0;
+        else q[7:0]<=d[7:0];
+    end
+endmodule
